7f6fe8dd4979597a6e91a273fffaab7af321c1fc
Sergey Shnaidman <sshnaidm@cisco.com>
2014-07-04 15:47:38 +0400

    Fixes

--- tools/deployers/install_devstack.py
+++ tools/deployers/install_devstack.py
@@ -79,10 +79,9 @@ def install_openstack(settings_dict, envs=None, verbose=None, prepare=False, for
             if not force and prepare:
                 return True
             elif not force and not prepare:
-                with cd("~/"):
-                    warn_if_fail(run("git clone https://github.com/openstack-dev/devstack.git"))
+                warn_if_fail(run("git clone https://github.com/openstack-dev/devstack.git"))
                 with cd("devstack"):
-                    make_local("~/devstack", "local.conf", False)
+                    make_local("devstack", "local.conf", False)
                     warn_if_fail(run("./stack.sh"))
             elif force:
                 shell_envs = ";".join(["export " + k + "=" + v for k, v in envs.iteritems()]) or ""
